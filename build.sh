4f208ad48a2f965dfb21efa7d86c4e1d7fa1fdc5
Tom Bentley <tbentley@redhat.com>
2021-04-30 11:27:58 +0100

    Avoid even try to build image when it's not changed

--- build.sh
+++ build.sh
@@ -1,6 +1,17 @@
 #! /bin/sh
 TAG=quay.io/tombentley/kafka-internals:latest
-mkdir output
-docker build --build-arg kafka_version=2.8.0 -t "$TAG" .
+if [ ! -e output ] 
+then
+  mkdir output
+fi
+
+IMAGE_TIME=.image
+
+if [ ! -e $IMAGE_TIME ] || [ Dockerfile -nt $IMAGE_TIME ]
+then 
+  docker build --build-arg kafka_version=2.8.0 -t "$TAG" .
+  touch $IMAGE_TIME
+fi 
+
 docker run --rm -v $(pwd)/output:/build/output:Z -v $(pwd)/src:/build/src:Z "$TAG" \
   asciidoctor -r asciidoctor-diagram -D output -R src src/master.adoc
